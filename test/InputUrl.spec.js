c20e660ba2a74f53d2787106bb5de1280c5a2777
Mirachle <cz.mirabella@freemail.hu>
2019-05-17 14:11:32 +0200

    test

--- test/InputUrl.spec.js
+++ test/InputUrl.spec.js
@@ -1,12 +1,23 @@
 import { mount } from '@vue/test-utils'
-import InputUrl from '../src/components/Input/InputUrl.vue'
+import InputRow from '../src/components/Input/InputRow.vue'
 
-describe('InputUrl', () => {
+describe('InputRow', () => {
     // Now mount the component and you have the wrapper
-    const wrapper = mount(InputUrl)
-    wrapper.setData({value: 'sajt'})
-    const value = wrapper.find('input').element.value
-    it('Szoveg egyezes', () => {
-        expect(value).toMatch('sajt')
+    const wrapper = mount(InputRow)
+    //wrapper.setData({value: 'sajt'})
+    //const value = wrapper.find('input').element.value
+    it('alap adat', () => {
+      expect(wrapper.vm.color).toBe("")
+      expect(wrapper.vm.disable).toBe(true)
+    })
+    it('rossz url', () => {
+      wrapper.vm.urlChanged("rossz")
+      expect(wrapper.vm.color).toBe("red-color")
+      expect(wrapper.vm.disable).toBe(true)
+    })
+    it('jó url', () => {
+      wrapper.vm.urlChanged('https://jsonplaceholder.typicode.com/posts')
+      expect(wrapper.vm.color).toBe("")
+      expect(wrapper.vm.disable).toBe(false)
     })
 })
